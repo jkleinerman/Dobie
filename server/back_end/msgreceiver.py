be9afa262ba6a80c5612cc04b7a15add90095636
Jorge Kleinerman <jkleinerman@gmail.com>
2016-05-23 16:59:45 -0300

    -Add MsgReceiver Thread to receive any type of message from controller -Add sendToCtrller method in NetMngr to send any type of message to controller -Fix the add passage msg sent to controller in NetPassage

---
+++ server/back_end/msgreceiver.py
@@ -0,0 +1,56 @@
+import pymysql
+import queue
+import logging
+
+import genmngr
+import database
+from config import *
+
+
+
+
+class MsgReceiver(genmngr.GenericMngr):
+
+    def __init__(self, exitFlag):
+
+        #Invoking the parent class constructor, specifying the thread name, 
+        #to have a understandable log file.
+        super().__init__('MsgReceiver', exitFlag)
+
+        self.dataBase = database.DataBase(DB_HOST, DB_USER, DB_PASSWD, DB_DATABASE)
+    
+        self.netToMsgRec = queue.Queue()
+
+
+
+
+
+    def run(self):
+        '''
+        This is the main method of the thread. Most of the time it is blocked waiting 
+        for queue messages coming from the "Network" thread.
+        '''
+
+        while True:
+            try:
+                #Blocking until Main thread sends an event or EXIT_CHECK_TIME expires 
+                events = self.netToMsgRec.get(timeout=EXIT_CHECK_TIME)
+                self.checkExit()
+                self.dataBase.saveEvents(events)
+
+            except queue.Empty:
+                #Cheking if Main thread ask as to finish.
+                self.checkExit()
+
+
+
+
+
+
+
+
+    #def __del__(self):
+   
+        #self.connection.commit() 
+        #self.connection.close()
+
